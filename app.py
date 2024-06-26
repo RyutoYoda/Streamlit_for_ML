d03bfad1afac63c2d25047a572fd725b1ef24ac9
RyutoYoda <98861847+RyutoYoda@users.noreply.github.com>
2024-06-26 13:45:14 +0900

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from sklearn.linear_model import LinearRegression, LogisticRegression
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 from sklearn.metrics import r2_score, mean_absolute_percentage_error, mean_squared_error
 import lightgbm as lgb
-from catboost import CatBoostRegressor, CatBoostClassifier
+from catboost import CatBoostClassifier
 from dotenv import load_dotenv
 import base64
 import joblib
